521e62617e57e605c84e2a4ff1f360954b4415a2
shaocongwang <31238613+shaocongwang@users.noreply.github.com>
2020-10-22 23:30:20 +0800

    first time commit
    Signed-off-by: shaocongwang <shaocongwang@126.com>

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,38 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+@RestController
+@Slf4j
+public class PaymentController {
+    @Resource
+    private PaymentService paymentService;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(@RequestBody Payment payment){
+        int result = paymentService.create(payment);
+        log.info("********添加结果:{}", result);
+        if(result > 0){
+            return new CommonResult(200, "添加成功!", result);
+        } else {
+            return new CommonResult(444, "添加失败!", null);
+        }
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPayment(@PathVariable("id")Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("********查询结果:{}", payment);
+        if(null != payment){
+            return new CommonResult(200, "查询成功", payment);
+        } else {
+            return new CommonResult(444, "没有对应记录，查询 ID:" + id, null);
+        }
+    }
+
+}
